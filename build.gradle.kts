be26a340fdbec8c1dde8a7521d65d7ff6a6455cb
Anarchise <omgfg@hotmail.co.uk>
2021-10-08 16:28:34 +0100

    URL Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     checkstyle
 }
 
-project.extra["GithubUrl"] = "https://github.com/<INSERT NAME>/<INSERT REPOSITORY>"
+project.extra["GithubUrl"] = "https://github.com/anarchise/aplugins"
 
 apply<BootstrapPlugin>()
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "external plugins example"
+rootProject.name = "aplugins"
 
 
 include(":askiller")
